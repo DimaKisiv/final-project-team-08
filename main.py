4cc4d8844e6a94b327f704d47f242c40c855ad8a
Denys Almazov <almazovdenys@gmail.com>
2024-08-14 00:35:27 +0300

    small optimization

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from constants.messages import Messages
 from helpers.parser import parse_input
 import services.command_registry as command_service
 
+
 def main():
     print(Messages.Wellcome)
-    #TODO replace dict with repository
-    #TODO load storage from the file
+    # TODO replace dict with repository
+    # TODO load storage from the file
     storage = {}
+    command_executor = command_service.create_command_executor(storage)
     while True:
         command, *args = parse_input(input(Messages.EnterACommand))
-        command_executor = command_service.create_command_executor(storage)
         print(command_executor(command, *args))
-        #TODO save storage to the file
+        # TODO save storage to the file
+
 
 if __name__ == "__main__":
     main()
